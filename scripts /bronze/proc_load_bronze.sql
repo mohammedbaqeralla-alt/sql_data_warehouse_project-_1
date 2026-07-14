9df04bd414ba8ce4030fca4179e150c4279b6cd6
mohammedbaqeralla <mohammedbaqeralla@gmail.com>
2026-07-14 07:32:09 +0300

    Update proc_load_bronze.sql

--- scripts /bronze/proc_load_bronze.sql
+++ scripts /bronze/proc_load_bronze.sql
@@ -31,7 +31,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.crm_cust_info;
 		BULK INSERT bronze.crm_cust_info
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\cust_info.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -44,7 +44,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.crm_prd_info;
 		BULK INSERT bronze.crm_prd_info
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\prd_info.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -57,7 +57,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.crm_sales_details;
 		BULK INSERT bronze.crm_sales_details
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\sales_details.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -70,7 +70,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.erp_CUST_AZ12;
 		BULK INSERT bronze.erp_CUST_AZ12
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\CUST_AZ12.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -83,7 +83,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.erp_LOC_A101;
 		BULK INSERT bronze.erp_LOC_A101
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\LOC_A101.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -96,7 +96,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.erp_PX_CAT_G1V2;
 		BULK INSERT bronze.erp_PX_CAT_G1V2
-		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\PX_CAT_G1V2.CSV'
+		FROM 'A:\sql fiel\sql-data-warehouse-project-main\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
